4d87dc38e4d0c598f94b2742b3cc4be33362997a
hsiaolann <hsiaolantsao@gmail.com>
2021-12-09 21:22:25 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant') 
+
+mongoose.connect('mongodb://localhost/restarant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected!')
+
+    Restaurant.create(restaurantList)
+    .then(() => {
+      console.log("restaurantSeeder done!")
+      db.close()
+    })
+    .catch(err => console.log(err))
+
+})
